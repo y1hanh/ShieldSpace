b492b63a944651b96c0e5b22b56290258fa10099
jiaying <jlin0158@student.monash.edu>
2025-04-03 16:03:19 +1100

    fix syntax in route

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './routes.ts';
 import { AuthProvider } from './context/AuthContext';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import '@mui/material/styles';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
