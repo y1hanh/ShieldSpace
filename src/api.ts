d6b66a76d9122cfd96c6c7aed1559c78750ef64f
Yihan Huang <yhua0252@student.monash.edu>
2025-04-15 16:13:56 +1000

    use server api

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,4 @@
-const API_BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://api.shieldspace.games';
+const API_BASE_URL = 'https://api.shieldspace.games';
 
 type Methond = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
